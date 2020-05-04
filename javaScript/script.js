3756684ceb3de42ff2f403b76b3cb8d64d0c61fe
abdirahman <27870abdirahmanabdidict19s@gmail.com>
2020-05-04 22:37:19 +0300

    add data type,function,loop arrays and control structure to the page

---
+++ javaScript/script.js
@@ -0,0 +1,15 @@
+prompt("Enter your birthday");
+let day=(((CC/4)-2*((-1)+((5*YY/4))+((26*(MM+1)/10))+DD)mod 7
+if(day>0 && day<31){
+  document.write("valid"<br />)
+}
+relse if (month>0 && mark<=){
+  document.write("valid"<br />)
+}else
+document.write("invalid<br />")
+
+var akanNames=["kwasi","kwadwo","abenaa","akua"]
+akanNames.forEach(function(akanname){
+return gender
+});
+alert("your name",+akanname+"!")
