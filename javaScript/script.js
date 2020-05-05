62633e74e523f7f1706a956c17d9481fa363a98d
abdirahman <27870abdirahmanabdidict19s@gmail.com>
2020-05-05 07:08:11 +0300

    add changes to the form and javaScript

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,12 +1,12 @@
 prompt("Enter your birthday");
 let day=(((CC/4)-2*((-1)+((5*YY/4))+((26*(MM+1)/10))+DD)mod 7
 if(day>0 && day<31){
-  document.write("valid"<br />)
+  document.getElementbyid("valid"<br />)
 }
 relse if (month>0 && mark<=){
-  document.write("valid"<br />)
+  document.getElementbyid("valid"<br />)
 }else
-document.write("invalid<br />")
+document.getElementbyid("invalid<br />")
 
 var akanNames=["kwasi","kwadwo","abenaa","akua"]
 akanNames.forEach(function(akanname){
